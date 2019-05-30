7e71bd46f8e4519a8996ddfd9544e1fe7452bc4e
Vipul Sharma <vipuls526@gmail.com>
2019-05-30 09:32:42 +0530

    Update forecast.js

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,14 +1,27 @@
 const request = require('request')
 
+/*
+* We will be receiving a callback function parameter with two arguments
+*/
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
+    /*
+    * Here destructuring is used
+    * url: url as the first object of the first request parameter
+    * 
+    * Again destructuring has been used in the second parameter as well
+    * {body} comes from response.body
+    */
     request({ url, json: true }, (error, { body }) => {
         if (error) {
+            // reposnse is undefined
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
+            // again response is undefined
             callback('Unable to find location', undefined)
         } else {
+            // error is undefined, the second argument is the reponse
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
